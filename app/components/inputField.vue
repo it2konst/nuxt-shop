47381752b4e5ebeace0ced1ec54f921b90dd76fa
Konst Berg <163849606+it2konst@users.noreply.github.com>
2025-10-23 16:52:43 +0700

    feat: update the main page with the addition of functionality and the… (#1)
    * feat: update the main page with the addition of functionality and the InputField component
    
    * Add new SVG icons for various functionalities including LinkedIn, Search, Twitter, User, and Logo components
    
    - Added LinkedIn icon (li.svg)
    - Added Search icon (search.svg)
    - Added Twitter icon (tw.svg)
    - Added User icon (user.svg)
    - Added Logo component (IconLogo.vue)

---
+++ app/components/inputField.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { variant } = defineProps<{
+  variant: "gray" | "black";
+}>();
+const data = defineModel<string>();
+</script>
+
+<template>
+  <input v-model="data" :class="[$style[variant], $style.input]" />
+</template>
+
+<style module>
+.input {
+  width: 100%;
+  padding: 12px 0;
+  background: none;
+  border: none;
+  color: var(--color-black);
+  font-size: 16px;
+  font-weight: 400;
+}
+.input::placeholder {
+  color: var(--color-dark-gray);
+}
+.gray {
+  border-bottom: 1px solid var(--color-gray);
+}
+.black {
+  border-bottom: 1px solid var(--color-black);
+}
+</style>

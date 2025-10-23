b2430c2b5bd6acc155418bffbb4f91dbf10551d2
it2konst <gorky17@tuta.io>
2025-10-23 16:52:43 +0700

    feat: update the main page with the addition of functionality and the… (#1)
    * feat: update the main page with the addition of functionality and the InputField component
    
    * Add new SVG icons for various functionalities including LinkedIn, Search, Twitter, User, and Logo components
    
    - Added LinkedIn icon (li.svg)
    - Added Search icon (search.svg)
    - Added Twitter icon (tw.svg)
    - Added User icon (user.svg)
    - Added Logo component (IconLogo.vue)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+function openAbout() {
+  navigateTo({ path: "/about" });
+}
+useSeoMeta({
+  title: "Главная магазина",
+  description: "Магазин Shoppe с ювелирными изделиями.",
+  ogDescription: "Магазин Shoppe с ювелирными изделиями.",
+});
+</script>
+
 <template>
-  <div>index</div>
+  <div>
+    Index
+    <NuxtLink to="/about">About</NuxtLink>
+    <button @click="openAbout">Перейти</button>
+    <InputField variant="black" placeholder="Введите ваш email для подписки" />
+  </div>
 </template>
-
-

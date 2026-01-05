1f58a83d4b6d4881573888374e7440ee7dfb01ab
it2konst <gorky17@tuta.io>
2026-01-05 22:32:10 +0700

    feat: restructure app layout and enhance page transition effects

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import "~/assets/styles/main.css"
+import "~/assets/styles/main.css";
 </script>
 
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <main class="main">
+      <NuxtPage
+        :transition="{
+          name: 'page',
+          mode: 'out-in',
+        }"
+      />
+    </main>
   </NuxtLayout>
 </template>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+
+.main {
+  margin: 0 auto;
+  max-width: 1248px;
+  padding: 40px 16px;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>

49b3a026421b8d351d475f0171a354aef4f3ad53
it2konst <gorky17@tuta.io>
2026-01-07 19:15:45 +0700

    feat: update the catalog page with search and filtering support, add interfaces for products

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,23 +1,104 @@
 <script setup lang="ts">
-  import type { GetCategoriesResponse } from "~/interfaces/category.interface";
-  
-  const config = useRuntimeConfig();
-  const API_URL = config.public.apiurl;
+import type { GetCategoriesResponse } from "~/interfaces/category.interface";
+import type { GetProductsResponse } from "~/interfaces/product.interface";
 
-  const { data, error } = await useFetch<GetCategoriesResponse>(`${API_URL}/api/categories.json`);
-  console.log(data.value);
-  console.log(error.value);
+useSeoMeta({
+  title: "Каталог товаров",
+  description: "Каталог товаров магазина Shoppe с ювелирными изделиями.",
+  ogDescription: "Каталог товаров магазина Shoppe с ювелирными изделиями.",
+});
 
-  // try {
-  //   const data = await $fetch<GetCategoriesResponse>(API_URL + '/api/categories');
-  //   console.log(data);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+// useHead({
+//   title: "Каталог товаров",
+//   meta: [
+//     {
+//       name: "description",
+//       content: "Каталог товаров магазина Shoppe с ювелирными изделиями.",
+//     },
+//   ],
+// });
+
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
+const route = useRoute();
+const category_id = ref(route.query.category_id?.toString() ?? "");
+const search = ref(route.query.search?.toString() ?? "");
+
+const query = computed(() => ({
+  limit: route.query.limit ?? 20,
+  offset: route.query.offset ?? 0,
+  category_id: route.query.category_id || undefined,
+  search: route.query.search || undefined,
+}));
+
+const { data } = await useFetch<GetCategoriesResponse>(`${API_URL}/api/categories.json`);
+
+const selectDefault = { value: "", label: "Категории" };
+const categoriesSelect = computed(() => {
+  return data.value
+    ? data.value?.categories
+        .map((c) => ({
+          value: c.id.toString(),
+          label: c.name,
+        }))
+        .concat(selectDefault)
+    : [selectDefault];
+});
+
+const { data: productsData } = await useFetch<GetProductsResponse>(
+  API_URL + "/products",
+  {
+    key: "get-products",
+    query,
+  }
+);
 </script>
 
 <template>
-  <div>catalog</div>
+  <div>
+    <h1 class="left">Каталог товаров</h1>
+    <div class="catalog">
+      <div class="catalog__filter">
+        <div class="catalog__search">
+          <InputField v-model="search" variant="gray" placeholder="Поиск..." />
+          <Icon name="icons:search" size="18px" />
+        </div>
+        <SelectField v-model="category_id" :options="categoriesSelect" />
+      </div>
+      <div class="catalog__grid">
+        <CatalogCard
+          v-for="product in productsData?.products"
+          :key="product.id"
+          v-bind="product"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-
+<style scoped>
+.catalog {
+  display: flex;
+  gap: 36px;
+}
+.catalog__filter {
+  width: 260px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+.catalog__grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 64px 12px;
+}
+.catalog__search {
+  position: relative;
+}
+.catalog__search .iconify {
+  position: absolute;
+  top: 12px;
+  right: 8px;
+}
+</style>

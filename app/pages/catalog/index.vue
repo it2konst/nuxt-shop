9d237c0c6780d3efd90e8d96ba0bc7fe15397715
Konstantin Bergamentov <wotrvs@gmail.com>
2026-01-07 18:12:04 +0700

    feat: adding category interfaces and loading data from the API

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+  import type { GetCategoriesResponse } from "~/interfaces/category.interface";
+  
+  const config = useRuntimeConfig();
+  const API_URL = config.public.apiurl;
+
+  const { data, error } = await useFetch<GetCategoriesResponse>(`${API_URL}/api/categories.json`);
+  console.log(data.value);
+  console.log(error.value);
+
+  // try {
+  //   const data = await $fetch<GetCategoriesResponse>(API_URL + '/api/categories');
+  //   console.log(data);
+  // } catch (error) {
+  //   console.log(error);
+  // }
+</script>
+
 <template>
   <div>catalog</div>
 </template>

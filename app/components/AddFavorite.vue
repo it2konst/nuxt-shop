d642558bb5d135233a1429fbfc10d14f73ce8320
it2konst <gorky17@tuta.io>
2026-01-10 22:47:59 +0700

    feat: adding the AddFavorite component and integrating it into the CatalogCard component

---
+++ app/components/AddFavorite.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { id, isShown } = defineProps<{
+  id: number;
+  isShown: boolean;
+}>();
+</script>
+
+<template>
+  <button
+    class="fav-button"
+  >
+    <Icon name="icons:favorite-add" size="18px" />
+  </button>
+</template>
+
+<style scoped>
+.fav-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
